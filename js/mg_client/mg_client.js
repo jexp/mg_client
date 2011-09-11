daa9849af92e624606c0fe64eb08dab82c561140
Michael Hunger <github@jexp.de>
2011-09-11 14:35:26 +0200

    Verarbeiten von finger und info Informationen

--- js/mg_client/mg_client.js
+++ js/mg_client/mg_client.js
@@ -1,12 +1,21 @@
 var triggers = {};	
 	
-function addTrigger(id,trigger) {
+function addTrigger(id, trigger) {
 	triggers[id] = trigger;
+}
+function addTriggers(id, actions) {
+	addTrigger(id, function(line) {
+		for (var i=0;i<actions.length;i++) {
+			line = actions[i](line);
+		}
+		return line;
+	});
 }
 
 // todo besseres handling, trigger auf uspruengliche zeile und delta's verwalten (aenderungen an zeile)
 // verwaltung von prios, fall-thru und farb/style infos in separatem kanal halten
 function runTriggers(line) { 
+	if (!line) return line;
 	for (name in triggers) {
 		line = triggers[name](line);
 	}
@@ -28,14 +37,14 @@ function collect(start,end, fun) { // todo objekt, mit start/end ausschluss, gag
 	var collected = null;
 	return function(line) {
 		if (line.match(start)) {
-			collected = "";
+			collected = [];
 		} 
-		if (collected != null) {
-			collected += line + "\n";
+		if (collected) {
+			collected.push(line);
 		}
 		if (line.match(end)) {
-			if (collected != null && collected.length > 0) {
-				fun(collected);
+			if (collected && collected.length > 0) {
+				fun(collected.join("\n"),collected);
 			}
 			collected = null;
 		}
@@ -215,13 +224,34 @@ function runScript() { // name, params = Array ?
 	})
 }
 
+function run(action,line, match,trigger) {
+	var type = typeof(action)
+	if (type == "string" ) return eval(action);
+	if (type == "function" ) {
+		var fullmatch = match.shift();
+		return action.call(this, { match : fullmatch, line:line, trigger:trigger, groups : match });
+	}
+	if (type == "object" ) {
+		for (p in action) {
+			if (action.hasOwnProperty(p)) {
+				Player[p]=action[p];
+			}
+		}
+	}
+}
+
 function highlight(trigger) {
 	return function(line) {
+		var match=line.match(trigger.trigger);
+		if (!match) return line;
+		if (trigger.action) {  run(trigger.action, line, match, trigger); }
+		if (!(trigger.style || trigger.click || trigger.dblclick)) return line;
+		
 		return line.replace(trigger.trigger, function (text) {
 			var span = $("<span>");
-			if (trigger.style!=null) { span.css(trigger.style); }
-			if (trigger.click!=null) { span.click(trigger.click(text)); }
-			if (trigger.dblclick!=null) { span.dblclick(trigger.dblclick(text));}
+			if (trigger.style) { span.css(trigger.style); }
+			if (trigger.click) { span.click(trigger.click(text)); }
+			if (trigger.dblclick) { span.dblclick(trigger.dblclick(text));}
 			span.text(text);
 			return span.wrap("<div>").parent().html();
 		});
--- js/mg_client/mg_client_player.js
+++ js/mg_client/mg_client_player.js
@@ -1,3 +1,5 @@
+// todo heartbeat functions with register
+
 /*
 Mesirii ist anwesend,
 und zwar von: Daemonendimension (ueber Berlin).
@@ -23,7 +25,7 @@ var Player = {
 	max_kp : 100,
 	vorsicht : 0,
 	flucht : null,
-	poison : null,
+	poison : 0,
 	xp : 0,
 	ap : 0,
 	max_ap : 0,
@@ -39,7 +41,11 @@ var Player = {
 	intellect : 0,
 	strength : 0,
 	dexterity : 0,
-	endurance : 0,
+	constitution : 0,
+    intellect_mod : 0,
+    strength_mod : 0,
+    dexterity_mod : 0,
+    constitution_mod : 0,
 	deaths : 0,
 	death_marks : 0,
 	age : null,
@@ -52,7 +58,6 @@ var Player = {
 	logged_in_since: null,
 	first_login : null,
 	married_to : null,
-	deaths: 0,
 	project : null,
 	second : false
 }
@@ -100,6 +105,7 @@ function showPlayer() {
 	$("#p_vorsicht").text(Player.vorsicht)
 	$("#p_flucht").text(Player.flucht)
 	$('#status').dialog("option", "title", Player.name );
+	$("#p_poison").text(Player.poison)
 }
 
 var grab_battle = grab_single(/(^  [^' ].+|.+ faellt tot zu Boden.$)/, function(text) { appendTo("p_fight",text); })
@@ -121,6 +127,35 @@ function add_player_connect_triggers() {
 		connectPlayer(name)
 	}));
 }
+
+var players = {};
+
+function property_update(trigger,props) {
+	var properties = Array.prototype.slice.call(arguments);
+	return function(player, line) {
+		var match;
+	  	if (match=line.match(trigger)) {
+	  		for (var i=1;i<match.length && i<properties.length; i++) {
+				var value=match[i];
+				if (value) {
+	  				var prop = properties[i];
+	  				console.log("Updating "+prop+" to "+value);
+	  				player[prop]=value;
+				}
+	  		}
+	  	}};
+}
+
+function lookup_player(name) {
+	if (name == Player.name) {
+		return Player;
+	} 
+	if (!players[name]) {
+		players[name] = { name : name};
+	} 
+	return players[name];
+}
+
 function add_player_triggers() {
 	console.log("add_player_triggers")
 
@@ -129,29 +164,121 @@ function add_player_triggers() {
 		Player.lp = lp;
 		Player.kp = kp;
 	}));
-
-	// todo handle complete finger result in one go
-	addTrigger("avatar",
-	trigger_update(/^Avatar-URI: (.+)$/,function(avatar) { 
-		Player.avatar = avatar;
+	
+	/*
+	und zwar von: Daemonendimension (ueber Berlin).
+	Eingeloggt seit: Son, 11. Sep 2011, 10:23:45
+	Voller Name: Chaosmacher Mesirii hat eine Elfentraube im Ruecken
+	Rasse: Zwerg,  Gilde: Chaos,  Geschlecht: maennlich
+	Alter: 4a 52d 16:52:34,   Spielerlevel: 113 (Seher),   Gildenlevel: 11
+	Datum des ersten Login: Mit,  9. Nov 1994, 16:22:47
+	Homepage: www.rana-elisabeth.de, http://mesirii.de, mud@mesirii.de
+	ICQ: 213864527
+	Verheiratet mit: Mekare
+	Mesirii ist Zweitspieler.
+	Bisher bereits 348 mal gestorben
+	Projekt: git clone https://github.com/jexp/TinyMacros.git
+	*/
+	var finger_checks = [
+		 property_update(/und zwar von: (.+)(?: \(ueber (.+)\))?\./,'from','via')
+		,property_update(/Eingeloggt seit: (.+)/,'logged_in_since')
+		,property_update(/Voller Name: (.+)/,'full_name')
+		,property_update(/Rasse: (.+),  Gilde: (.+),  Geschlecht: (.+)/,'race','guild','gender')
+		,property_update(/(?:Alter: .+, +)?Spielerlevel: (\d+)(?: \((Seher|Magier)\))?, +Gildenlevel: (\d+)/,'level','hlp','guild_level')
+		,property_update(/Datum des ersten Login: (.+)/, 'first_login')
+		,property_update(/Verheiratet mit: (.+)/,'married_to')
+		,property_update(/.+ ist Zweitspieler\./,'second')
+		,property_update(/Bisher bereits (\d+) mal gestorben/,'deaths')
+		,property_update(/Projekt: (.)/,'project')
+		,property_update(/Avatar-URI: (.+)/,'avatar')
+	];
+	
+	addTrigger("finger",
+	   collect(/^.+ ist anwesend,$/, /^\S*>\s*$/, function(text,lines) {
+		var player,match;
+		if (match = text.match(/^(.+) ist anwesend,\n/)) {
+		   var name = match[1];
+		   player = lookup_player(name)
+		}
+		for (var i=0;i<finger_checks.length;i++) {
+			for (var j=0;j<lines.length;j++) {
+				finger_checks[i](player, lines[j]);
+			}
+		}
+		console.log("finger: "+player.name+" av"+player.avatar);
 	}));
 
-	addTrigger("teddy_vorsicht",
-	trigger_update(/^Vorsicht: (.+). Fluchtrichtung: (.+)$/,function(vorsicht,flucht) { 
-		Player.vorsicht = vorsicht;
-		Player.flucht = flucht;
+/*
+- Gast5 der hoffnungsvolle Anfaenger. --------------------------------
+Rasse ............ Mensch             Abenteuer ........ 0 
+Geschlecht ....... maennlich          Groesse .......... 164 cm
+Stufe ............ 1   (6)            Gewicht .......... 74 kg
+Gilde ............ Abenteurer         Gildenstufe ...... 1
+Erfahrung ........ 0 Punkte           Charakter ........ neutral
+
+Ausdauer .........  1 (+1)            Geschicklichkeit .  1 (+3)
+Kraft ............  1 (+1)            Intelligenz ......  1 (+1)
+
+Gesundheit ....... 58                 Gift ............. gesund
+Konzentration .... 58                 Vorsicht ......... 20
+Todesfolgen....... kein Malus         
+Du kennst Dich im MorgenGrauen so gut wie gar nicht aus. 
+Alter:	1 Stunde 10 Minuten 36 Sekunden.
+*/
+	var info_checks = [
+	property_update(/Rasse ............ (.+)             Abenteuer ........ (\d+)(?: \((\d+)\))?/,'race','ap','max_ap')
+	,property_update(/Geschlecht ....... (.+)          Groesse .......... (\d+) cm/,'gender','size')
+	,property_update(/Stufe ............ (\d+)(?: +\((\d+)\)) +Gewicht .......... (\d+) kg/,'level','max_level','weight')
+	,property_update(/Gilde ............ (.+)         Gildenstufe ...... (\d+)(?: \((\d+)\))?/,'guild','guild_level','max_guild_level')
+	,property_update(/Erfahrung ........ (\d+) Punkte           Charakter ........ (.+)/,'xp','align')
+	,property_update(/Ausdauer ......... +(\d+)(?: \(([+-]\d+)\)) +Geschicklichkeit .  (\d+)(?: \(([+-]\d+)\))/,'constitution','constitution_mod','dexterity','dexterity_mod')
+	,property_update(/Kraft ............ +(\d+)(?: \(([+-]\d+)\)) +Intelligenz ......  (\d+)(?: \(([+-]\d+)\))/,'strength','strength','intellect','intellect_mod')
+	,property_update(/Gesundheit ....... (\d+)(?: +\((\d+)\))? +Gift ............. (gesund|leicht|gefaehrlich)/,'lp','max_lp','poison')
+	,property_update(/Konzentration .... (\d+)(?: +\((\d+)\))? +Vorsicht ......... (\d+|mutig)/,'kp','max_kp','vorsicht')
+	,property_update(/Todesfolgen....... (\d|kein Malus)/,'death_marks')
+	,property_update(/Du kennst Dich im MorgenGrauen (.+)/,'fp')
+//	property_update(/Rasse ............ (.+)             Abenteuer ........ (\d+)(?: \((\d+)\))?/,'race','ap','max_ap')
+	
+	];
+
+	addTrigger("info",
+	   collect(/- .+ -{3,}$/, /----------------------------------------------------------------------/, function(text,lines) {
+		var player = Player;
+		for (var i=0;i<info_checks.length;i++) {
+			for (var j=0;j<lines.length;j++) {
+				info_checks[i](player, lines[j]);
+			}
+		}
+		console.log("info: "+player.name+" av"+player.avatar);
 	}));
 
-	addTrigger("vorsicht",
-	trigger_update(/^Vorsicht-Modus \((.+)\)$/,function(vorsicht) { 
-		Player.vorsicht = vorsicht;
-	}));
-	addTrigger("vorsicht0",
-	trigger_update(/^Prinz Eisenherz-Modus.$/,function() { 
-		Player.vorsicht = 0;
-	}));
-	addTrigger("kurzinfo_kp",
-	trigger_update(/Konzentration: 0 \|.+\| +(\d+) \((\d+)\)$/,function(val,max) { Player.kp = max; Player.max_kp = max; }));
+    addTriggers("gift",[
+		highlight({trigger:/Du hast eine leichte Vergiftung./, style: {color:"yellow"}, action : function() { Player.poison = 1 }}),
+		highlight({trigger:/Du hast eine schwere Vergiftung./, style: {color:"red"},  action : { poison : 2 }}),
+		highlight({trigger:/Du hast eine gefaehrliche Vergiftung./, style: {color:"red", "font-weight" :"bold"}, action : "Player.poison = 3 "})
+	]);
+
+	addTriggers("kurzinfo",[
+		highlight({ trigger: /Konzentration: 0 \|.+\| +(\d+) \((\d+)\)$/, action : function(args) { 
+			Player.kp = args.groups[0]; Player.max_kp = args.groups[1]; 
+		}})
+	]);
+	
+	addTriggers("vorsicht",[
+  	trigger_update(/^Vorsicht: (.+). Fluchtrichtung: (.+)$/,function(vorsicht,flucht) { 
+  		Player.vorsicht = vorsicht;
+  		Player.flucht = flucht;
+  	}),
+  	trigger_update(/^Vorsicht-Modus \((.+)\)$/,function(vorsicht) { 
+  		Player.vorsicht = vorsicht;
+  	}),
+  	trigger_update(/^Prinz Eisenherz-Modus.$/,function() { 
+  		Player.vorsicht = 0;
+  	})	
+ 	]);
+
+//	addTrigger("kurzinfo_kp",
+//	trigger_update(/Konzentration: 0 \|.+\| +(\d+) \((\d+)\)$/,function(val,max) { Player.kp = max; Player.max_kp = max; }));
 	addTrigger("kurzinfo_lp",
 	trigger_update(/Gesundheit:    0 \|.+\| +(\d+) \((\d+)\)$/,function(val,max) { Player.lp = val; Player.max_lp = max; }));
 	addTrigger("kurzinfo_kp_max",
